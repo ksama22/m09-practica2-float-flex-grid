00cf114cb895e44dee3c1fd1a8e1013f751cc58e
ksama22 <ksama22@jaumebalmes.net>
2022-11-04 17:34:44 +0100

    First commit

---
+++ images/ejemplo.js
@@ -0,0 +1,19 @@
+getElementByTagNameTable= ["table", "table", "table", "table"];
+
+
+
+getElementByIdPutiClub.removeAllChildren()
+
+console.log(getElementByIdPutiClub.length);
+console.log(getElementByIdPutiClub);
+
+
+for (let i = 0; i < getElementByIdPutiClub.length; i++) {
+    getElementByIdPutiClub[i].remove()
+
+
+}
+
+
+
+getElementByTagNameTable[0
